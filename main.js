bf3488d0f37104b8ba2bfefb03120a593db0e901
Vadim Korotov <mpcomp12@gmail.com>
2021-12-12 11:54:54 +0300

    Refactoring and Bug fixed. Helpers added

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const fighters =  {
+const FIGHTERS =  {
     scorpion : {
         name : 'Scorpion',
-        hp :   100,
         img:   './assets/fighters/scorpion.gif',
         weapon: ['axe'],
         attack: function() {
@@ -10,7 +9,6 @@ const fighters =  {
     },
     liukang : {
         name : 'Liu Kang',
-        hp :   90,
         img:   './assets/fighters/liukang.gif',
         weapon: ['knife'],
         attack: function() {
@@ -18,27 +16,57 @@ const fighters =  {
         }
     }
 }
-function createPlayer(playerClass, fighter) {
-    const $player = document.createElement('div');
-    $player.classList.add(playerClass);
+const $arena = document.querySelector('.arenas');
+const player1 = {
+    player: 1,
+    fighter: FIGHTERS.scorpion,
+    hp: 100
+}
+const player2 = {
+    player: 2,
+    fighter: FIGHTERS.liukang,
+    hp: 100
+}
+// Helper functions
 
-    const $progressBar = document.createElement('div');
-    $progressBar.classList.add('progressbar');
+//Возвращает случайное целое число в диапазоне [min,max]
+function getRandInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+  }
+// возращает HTMLElement с классами, по emmet подобному синтаксису p.myclass -> <p class="myclass">
+// .some -> <div class = "some" 
+createElement = function(emmet) {
+    emmet = emmet.trim();
+    let emmetList = emmet.split('.');
+    let $el = (emmet[0] != '.') ? document.createElement(emmetList[0].trim())
+                                : document.createElement('div')
+    emmetList.shift();
+    if (emmetList.length === 0) {
+        return $el;
+    }
+    for (className of emmetList) {
+        $el.classList.add(className.trim());
+    }
+    return $el;
+} 
 
-    const $life = document.createElement('div');
-    $life.classList.add('life');
-    $life.style.width = fighter.hp;
-
-    const $name = document.createElement('div');
-    $name.innerText = fighter.name;
-
+function createPlayer(player) {
+    let playerClass = 'player' + player.player;
+    const $player = createElement('.' + playerClass);
+    
+    const $progressBar = createElement('.progressbar');
+    const $life = createElement('.life');
+    $life.style.width = player.hp + '%';
+    const $name = createElement('.name');
+    $name.innerText = player.fighter.name;
     $progressBar.appendChild($life);
     $progressBar.appendChild($name);
 
-    const $character = document.createElement('div');
-    $character.classList.add('character');
-    const $img = document.createElement('img');
-    $img.src = fighter.img;
+    const $character = createElement('.character');
+    const $img = createElement('img');
+    $img.src = player.fighter.img;
     $character.appendChild($img);
 
     $player.appendChild($progressBar);
@@ -46,10 +74,10 @@ function createPlayer(playerClass, fighter) {
 
     return $player;
 }
-const $arena = document.querySelector('.arenas')
+
 $arena.appendChild(
-    createPlayer('player1', fighters.scorpion)
+    createPlayer(player1)
     );
 $arena.appendChild(
-    createPlayer('player2', fighters.liukang)
+    createPlayer(player2)
 )
